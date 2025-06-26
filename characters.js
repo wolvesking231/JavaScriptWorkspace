0e77f5c4b1af77a9dbb81a01735a13cc01bf0cf1
wolvesking231 <hunter.hillx@gmail.com>
2025-06-26 16:32:47 -0700

    Character Manipulation

---
+++ characters.js
@@ -0,0 +1,24 @@
+let randomLetters = "pdfsdj";
+console.log(randomLetters.charAt(4));
+
+let houseNumber = "555";
+let streetName = "Shady Lane";
+console.log(houseNumber.concat(" ", streetName));
+
+let typeOfTree = "Pine";
+console.log(typeOfTree.indexOf("e"));
+
+let vowelsList = "a, e, i, o, u";
+console.log(vowelsList.split(","));
+
+let phoneNumber = "313-555-1234";
+console.log(phoneNumber.substring(12, 4));
+
+let message = "Learn FORTRAN.";
+console.log(message.replace("FORTRAN", "JavaScript"));
+
+let username = "ChrisMinnick";
+console.log(username.toLowerCase());
+
+let stateName = "washington";
+console.log(stateName.toUpperCase());
